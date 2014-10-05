145a6c8d2cfdaf6d3cf205b24e1fcc8579237e43
Dave Cohen <dave@project768.com>
2014-10-05 22:29:25 +0100

    -improve show beer screen after creation, i.e. "send this link to your friend!" -offered beers now show on user homepage

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,33 @@
 class UserController < ApplicationController
+  
+  def last5(coll)
+    if coll.count < 5
+      return coll
+    else
+      return coll[-5..-1]
+    end
+  end
+
+
   def home
     @user = current_user
     
     # put the 5 newest beer objects into a collection 
     @beers_given = []
-
-    unless @user.beersGiven.count < 5
-      @user.beersGiven[-5..-1].each do |b|
+    last5(@user.beersGiven).each do |b|
         bObj = Beer.find_by id: b
         @beers_given << bObj
-      end
     end
 
     # count received beers
     @received_count = @user.beersReceived.count
 
-    # count offered beers
-    @beersOffered = @user.beersOffered.count
+    # show offered beers
+    @beersOffered = []
+    @user.beersOffered.each do |b|
+      bObj = Beer.find_by randID: b
+      @beersOffered << bObj
+    end
 
     #identity has email addr
     identity = Identity.find(current_user.uid.to_i)
